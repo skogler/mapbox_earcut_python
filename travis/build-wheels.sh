6c0db2bf03d81b2e36d0c18749b02082522666dc
Samuel Kogler <samuel.kogler@gmail.com>
2018-07-25 14:01:43 +0200

    Only build python3

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -9,8 +9,11 @@ PY_VER_PATCH=0
 "/opt/python/cp${PY_VER_MAJOR}${PY_VER_MINOR}-cp${PY_VER_MAJOR}${PY_VER_MINOR}m/bin/pip" install cmake
 export PATH="/opt/_internal/cpython-${PY_VER_MAJOR}.${PY_VER_MINOR}.${PY_VER_PATCH}/bin:${PATH}"
 
+PYTHON_VERSIONS=(37 36 35)
+
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for VERSION in "${PYTHON_VERSIONS[@]}"; do
+    PYBIN="/opt/python/cp${VERSION}-cp${VERSION}m/bin"
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
@@ -20,6 +23,7 @@ for whl in wheelhouse/*.whl; do
     auditwheel repair "$whl" -w /io/wheelhouse/
 done
 
-for PYBIN in /opt/python/*/bin/; do
+for VERSION in "${PYTHON_VERSIONS[@]}"; do
+    PYBIN="/opt/python/cp${VERSION}-cp${VERSION}m/bin"
     "${PYBIN}/pip" install mapbox_earcut --no-index -f /io/wheelhouse
 done
